a7ae92782be3f26a4ba57b32d607ed3af3bcee86
MubashirMM <32304mubashir@gmail.com>
2026-04-29 21:53:52 +0500

    voice interated for bills and reports

--- myapp/api/ai_models.py
+++ myapp/api/ai_models.py
@@ -5,6 +5,10 @@ from myapp.crud.ai_models.ai_models_items import process_voice_items
 from myapp.crud.ai_models.ai_models_cart import process_voice_cart
 from myapp.crud.ai_models.ai_models_udhaar_items import process_voice_udhaar_items
 from myapp.crud.ai_models.ai_models_udhaars import process_voice_udhaars
+from myapp.crud.ai_models.ai_models_bills import process_voice_bills
+from myapp.crud.ai_models.ai_models_reports import process_voice_reports
+
+
 
 
 from myapp.utils.security import get_current_user
@@ -103,6 +107,56 @@ async def voice_process_endpoint(
     """
     try:
         result = await process_voice_udhaars(payload.audio_base64, current_user, db)
+        
+        # Check if result contains error
+        if isinstance(result, dict) and "error" in result:
+            raise HTTPException(status_code=400, detail=result)
+        
+        return result
+        
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        print(f"Error in voice_process_endpoint: {str(e)}")
+        raise HTTPException(status_code=400, detail={"error": "آڈیو پروسیس کرنے میں خرابی ہوئی۔ دوبارہ کوشش کریں۔"})
+    
+           
+@router.post("/voice-process-bills")
+async def voice_process_endpoint(
+    payload: VoiceItemsRequest,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    """
+    آواز سے آئٹمز پروسیس کریں - بیس 64 آڈیو بھیجیں (وائس لاگین کی طرح)
+    """
+    try:
+        result = await process_voice_bills(payload.audio_base64, current_user, db)
+        
+        # Check if result contains error
+        if isinstance(result, dict) and "error" in result:
+            raise HTTPException(status_code=400, detail=result)
+        
+        return result
+        
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        print(f"Error in voice_process_endpoint: {str(e)}")
+        raise HTTPException(status_code=400, detail={"error": "آڈیو پروسیس کرنے میں خرابی ہوئی۔ دوبارہ کوشش کریں۔"})
+
+                   
+@router.post("/voice-process-reports")
+async def voice_process_endpoint(
+    payload: VoiceItemsRequest,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    """
+    آواز سے آئٹمز پروسیس کریں - بیس 64 آڈیو بھیجیں (وائس لاگین کی طرح)
+    """
+    try:
+        result = await process_voice_reports(payload.audio_base64, current_user, db)
         
         # Check if result contains error
         if isinstance(result, dict) and "error" in result:
